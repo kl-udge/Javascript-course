394256f40befcb9fe6f69b9d866a851e3aec01b6
kludge <parmenasmunene165@gmail.com>
2024-08-13 23:02:48 +0300

    variables, if statements and boolean values

---
+++ lesson-01/js/index.js
@@ -0,0 +1,12 @@
+// JS basics
+
+alert('Hello world');
+
+alert('My first Js code');
+
+
+// maths
+
+2.2 + 3
+
+console.log(Math.log((4.32 * 42) * 3) / 5 );
---
+++ lesson-04/js/index.js
@@ -0,0 +1,7 @@
+// ternary operators
+
+true ? "truthy" : "falsey";
+
+const result = true ? "truthy" : "falsey";
+
+const output = computerMove === 'rock' ? result = 'rock' : 'Failed attempt';
